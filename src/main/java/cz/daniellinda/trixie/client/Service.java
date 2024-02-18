9477a081b12e68c8fa3753d1dc5648ef6da5b10f
WMeindW <daniellinda2005@gmail.com>
2024-02-18 14:38:51 +0100

    service logging

--- src/main/java/cz/daniellinda/trixie/client/Service.java
+++ src/main/java/cz/daniellinda/trixie/client/Service.java
@@ -1,9 +1,21 @@
 package cz.daniellinda.trixie.client;
 
+import cz.daniellinda.trixie.log.Logger;
+
 public class Service {
     public static void start() {
-        if (HttpClient.download("https://www.smartform.cz/download/kopidlno.xml.zip"))
-            if (Parser.unzip())
-                Parser.parseXml();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            Logger.saveLog("Shutting down");
+        }));
+        Logger.saveLog("Starting");
+        if (HttpClient.download("https://www.smartform.cz/download/kopidlno.xml.zip")) {
+            Logger.saveLog("Downloaded");
+            if (Parser.unzip()) {
+                Logger.saveLog("Unzipped");
+                if (Parser.parseXml()) {
+                    Logger.saveLog("Saved");
+                }
+            }
+        }
     }
 }
